62d903a734352e408cf66b35bdae3375a2cee3a5
Askh <149861091+Xaxathon@users.noreply.github.com>
2025-12-06 18:41:01 +0300

    uptate

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -276,7 +276,15 @@ export default function Home() {
 					<h2 className="text-center text-3xl sm:text-5xl font-semibold">
 						Мы на карте
 					</h2>
-					<div className="h-72 sm:h-134 w-full rounded-[3rem] bg-gray-300"></div>
+					<div className="relative h-[clamp(340px,35vw,640px)] w-full overflow-hidden rounded-3xl lg:rounded-4xl">
+						<iframe
+							title="Карта: Уфа, улица Карла Маркса, 60/2"
+							src="https://yandex.ru/map-widget/v1/-/CLg0REo8"
+							className="absolute inset-0 h-full w-full border-0"
+							allowFullScreen
+							referrerPolicy="no-referrer-when-downgrade"
+						/>
+					</div>
 				</section>
 			</main>
 
